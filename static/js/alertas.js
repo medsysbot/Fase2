ed6d624bed166a0974ffa268d59afeb1c519c8ff
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-05-05 20:13:37 -0400

    Update alertas.js

--- static/js/alertas.js
+++ static/js/alertas.js
@@ -1,13 +1,16 @@
 function showAlert(type, message, withButtons = false) {
   const iconos = {
-    alerta: "static/icons/alerta/alerta-alerta.png",
-    borrado: "static/icons/alerta/alerta-borrado.png",
-    busqueda: "static/icons/alerta/alerta-busqueda.png",
-    error: "static/icons/alerta/alerta-error.png",
-    guardado: "static/icons/alerta/alerta-guardado.png",
-    suceso: "static/icons/alerta/alerta-suceso.png",
-    pacienteCargado: "static/icons/alerta/alerta-paciente-cargado.png"
-  };
+  alerta: "static/icons/alerta/alerta-alerta.png",
+  borrado: "static/icons/alerta/alerta-borrado.png",
+  busqueda: "static/icons/alerta/alerta-busqueda.png",
+  error: "static/icons/alerta/alerta-error.png",
+  guardado: "static/icons/alerta/alerta-guardado.png",
+  suceso: "static/icons/alerta/alerta-suceso.png",
+  pacienteCargado: "static/icons/alerta/alerta-paciente-cargado.png",
+  pdf: "static/icons/alerta/alerta-pdf.png",
+  botonBorrar: "static/icons/alerta/alerta-boton-borrar.png",
+  botonNo: "static/icons/alerta/alerta-boton-no.png"
+};
 
   document.getElementById("alert-icon").src = iconos[type] || iconos["alerta"];
   document.getElementById("alert-text").textContent = message;

94d0c0bb76c6aeeb30e700c0f023fc72b046f147
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-05-23 21:16:07 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,8 @@ from .pdf_generator import (
     generar_pdf_resumen,
     generar_pdf_paciente,
     generar_pdf_historia_completa,
-)from .email_sender import enviar_email_con_pdf
+)
+from .email_sender import enviar_email_con_pdf
 
 __all__ = [
     "generar_pdf_resumen",

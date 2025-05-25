0d4c5162106906e065214987b48ebfad692de68f
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-05-25 19:07:16 -0300

    Remove Florencia invitation

--- routes/invitaciones.py
+++ routes/invitaciones.py
@@ -5,14 +5,8 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/tarjeta-florencia", response_class=HTMLResponse)
-async def tarjeta_florencia(request: Request):
-    return templates.TemplateResponse("tarjeta_florencia.html", {"request": request})
 
 @router.get("/tarjeta-marialuz", response_class=HTMLResponse)
 async def tarjeta_marialuz(request: Request):
     return templates.TemplateResponse("tarjeta_marialuz.html", {"request": request})
 
-@router.get("/confirmacion-florencia", response_class=HTMLResponse)
-async def confirmacion_florencia(request: Request):
-    return templates.TemplateResponse("confirmacion_florencia.html", {"request": request})

5f3c0c4e43fe737e911306c956c10d83d45b2642
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-05-30 16:33:31 -0400

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -9,6 +9,7 @@ from .acciones_busqueda import router as busqueda_router
 from .acciones_estudios import router as estudios_router
 from .acciones_admin import router as admin_router
 from .paginas import router as paginas_router
+from .solicitar_turno_publico import router as solicitar_turno_publico_router
 
 __all__ = [
     "pacientes_router",
@@ -22,5 +23,6 @@ __all__ = [
     "estudios_router",
     "admin_router",
     "paginas_router",
+    "solicitar_turno_publico_router",    
 ]
 

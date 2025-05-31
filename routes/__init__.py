72d65372377bb7360af05c0a1b20e5de0273972f
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-05-31 20:20:47 -0300

    Fix imports for receta PDFs

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,6 @@
 from .acciones_pacientes import router as pacientes_router
 from .acciones_historia_clinica_resumida import router as historia_clinica_resumida_router
-from .acciones_receta_medica import router as receta_medica_router
+from .acciones_receta import router as receta_medica_router
 from .acciones_historia_clinica import router as historia_clinica_router
 from .acciones_indicaciones_medicas import router as indicaciones_medicas_router
 from .acciones_consulta_diaria import router as consulta_diaria_router
--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,7 @@ from .pdf_generator import (
     generar_pdf_resumen,
     generar_pdf_paciente,
     generar_pdf_historia_completa,
-    generar_pdf_receta_medica,
+    generar_pdf_receta,
 )
 from .email_sender import enviar_email_con_pdf, enviar_email_simple
 from .image_utils import (
@@ -15,7 +15,7 @@ __all__ = [
     "generar_pdf_resumen",
     "generar_pdf_paciente",
     "generar_pdf_historia_completa",
-    "generar_pdf_receta_medica",
+    "generar_pdf_receta",
     "enviar_email_con_pdf",
     "enviar_email_simple",
     "guardar_imagen_temporal",

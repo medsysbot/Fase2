ab41d3140266b06b01a1c3999f19ca2840854c76
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-06-02 02:36:37 -0400

    Update turnos_pacientes.sql

--- sql/turnos_pacientes.sql
+++ sql/turnos_pacientes.sql
@@ -8,6 +8,8 @@ create table public.turnos_pacientes (
   fecha date not null,
   hora text not null,
   observaciones text,
+  nombre text not null,
+  apellido text not null
   pdf_url text,
   created_at timestamp without time zone default CURRENT_TIMESTAMP,
   constraint turnos_pacientes_institucion_id_fkey foreign key (institucion_id) references instituciones (id) on delete cascade

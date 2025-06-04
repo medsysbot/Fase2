6e235f6802a76737b3f66e3554185939028f5522
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-06-04 14:37:53 -0300

    [AG-05] Ajuste flujo turnos público

--- sql/turnos_pacientes.sql
+++ sql/turnos_pacientes.sql
@@ -1,16 +1,14 @@
 create table public.turnos_pacientes (
   id serial primary key,
   dni text not null,
-  institucion_id integer not null,
-  usuario_id text not null,
   profesional text not null,
   especialidad text not null,
   fecha date not null,
   hora text not null,
   observaciones text,
   nombre text not null,
-  apellido text not null
+  apellido text not null,
+  institucion_nombre text not null,
   pdf_url text,
-  created_at timestamp without time zone default CURRENT_TIMESTAMP,
-  constraint turnos_pacientes_institucion_id_fkey foreign key (institucion_id) references instituciones (id) on delete cascade
+  created_at timestamp without time zone default CURRENT_TIMESTAMP
 );
